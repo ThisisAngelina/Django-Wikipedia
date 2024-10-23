23f16d425a285ea55f1935bada58ffe6f81907a9
Angelina <angelinachigrinets@gmail.com>
2024-10-23 11:01:59 +0200

    complete project

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -30,7 +30,6 @@ def read_entry(request, title):
 def search(request):
     if request.method == 'POST':
         term = request.POST.get('search_form') #escaping special characters, just in case 
-        print('the search term is', term)
         if term and term != "": #if a search term exists and it is not an empty string:
             #get the list of entries
             entries = util.list_entries()
@@ -40,18 +39,16 @@ def search(request):
             for entry in entries:
                 #if it is a complete entry name (CASE-INCENSITIVE), send the user to the exact page
                 if re.fullmatch(term, entry, re.IGNORECASE):
-                    print("there was a full match")
                     return redirect('read_entry', title=entry)
            
             #else if if it a partial match, send the user to the list of possible matches. Clicking on any of the entry names on the search results page should take the user to that entry’s page.
                 elif re.search(term, entry, re.IGNORECASE):
                     matched_entries.append(entry)
-                    print("there was a partial match")
                 
             #after we finish looping through the existing entries, redierect to the list of matches. if the list is empty, redict to the home page.
             if len(matched_entries) > 0:
                 return render(request,'encyclopedia/search_results.html', {'matched_entries' : matched_entries}) 
-            else: #there are no matches - #TODO display an alert 
+            else: #there are no matches - #d redirect to the home page and isplay an alert 
                 message = "Oops there seems to be no such entry. But there are these ones that might interest you:"
                 return render(request, 'encyclopedia/index.html', {'message' : message,  "entries": util.list_entries()})
         #else - the search term was empty or not valid
@@ -68,7 +65,6 @@ def create_entry(request):
         content =  request.POST.get('content')
         #preparing the list of existing enty titles (needed to ensure that the user does not create a duplicate of an existing entry)
         existing_titles = util.list_entries()
-        print('existing titles are', existing_titles)
         #if the entry title or the entry content are missing, redirect the user to the same page with a message
         if not title or not content:
             message = "Oops! An entry title and content are required!"
@@ -78,7 +74,8 @@ def create_entry(request):
             message = "Oops! An entry with this title already exists!"
             return render(request, 'encyclopedia/create_entry.html', {'message' : message})
         else: #if all goes well - save the entry and redirect the user to the new entry’s page.
-            util.save_entry(title,content)
+            util.save_entry(title,content) 
+            messages.success(request, 'Entry saved successfully') #this is a better way to pass messages - standard for Django
             return redirect('read_entry', title=title)
     #if the request method is GET and the user just wants to see the form
     else:
@@ -115,7 +112,6 @@ def read_random(request):
     existing_entries = util.list_entries()
     index = random.randint(0, len(existing_entries)-1)
     chosen_entry = existing_entries[index]
-    print("the title of the chosen entry is", chosen_entry)
     md = util.get_entry(chosen_entry) #get title from the url, use the pre-made function to obtain the requested entry from the set of existing entries .
     content = markdown2.markdown(md)# convert md into HTML using markdown2. NOTE had to add the 'safe' ttribute to my DTL variable 'content' to have the HTML template render the HTML correctly
     return render(request, "encyclopedia/read_entry.html", {'content': content, 'title': chosen_entry})
